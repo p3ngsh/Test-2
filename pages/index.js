c20f218f762c36d86ff0bf06b459dba6fa7a2318
p3ngsh <113757775+p3ngsh@users.noreply.github.com>
2022-09-18 13:32:26 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="The 1st Makerspace in Mauritius" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
